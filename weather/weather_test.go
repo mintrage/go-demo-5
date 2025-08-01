185ab9700a012e815d1f97ccb60c0c29757d4c40
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-08-01 18:09:53 +0300

    16.7. Группы тестов Тесты

--- weather/weather_test.go
+++ weather/weather_test.go
@@ -23,14 +23,27 @@ func TestGetWeather(t *testing.T) {
 
 }
 
+var testCases = []struct {
+	name   string
+	format int
+}{
+	{name: "Big format", format: 147},
+	{name: "0 format", format: 0},
+	{name: "Minus format", format: -1},
+}
+
 func TestGetWeatherWrongFormat(t *testing.T) {
-	expected := "Moscow"
-	geoData := geo.GeoData{
-		City: expected,
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			expected := "Moscow"
+			geoData := geo.GeoData{
+				City: expected,
+			}
+			_, err := weather.GetWeather(geoData, tc.format)
+			if err != weather.ErrWrongFormat {
+				t.Errorf("Ожидалось %v, получено %v", weather.ErrWrongFormat, err)
+			}
+		})
 	}
-	format := 125
-	_, err := weather.GetWeather(geoData, format)
-	if err != weather.ErrWrongFormat {
-		t.Errorf("Ожидалось %v, получено %v", weather.ErrWrongFormat, err)
-	}
+
 }
